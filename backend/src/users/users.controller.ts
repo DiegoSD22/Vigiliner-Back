3a63b3b555b8ee0fd94a9dc7672e790805303e2c
Alejandro Gómez <alejandrogomez2339@gmail.com>
2026-02-24 15:29:49 -0500

    create user

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,6 @@
 // src/users/users.controller.ts
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -8,7 +9,14 @@ import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('users')
 export class UsersController {
+
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('ADMIN', 'USER')
   @Get()
@@ -21,8 +29,4 @@ export class UsersController {
     return this.usersService.findById(id);
   }
 
-  @Post()
-  create(@Body() body: any) {
-    return this.usersService.create(body);
-  }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const userCreated = await this.prisma.user.create({ data: { ...data, password: hashedPassword } });
+    const { password, ...userWithoutPassword } = userCreated;
+    return userWithoutPassword;
+  }
 
   findAll() {
     return this.prisma.user.findMany();
@@ -18,8 +26,4 @@ export class UsersService {
     return this.prisma.user.findUnique({ where: { email } });
   }
 
-  async create(data: { email: string; name: string; password: string }) {
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-    return this.prisma.user.create({ data: { ...data, password: hashedPassword } });
-  }
 }
